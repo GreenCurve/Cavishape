279d8b1bd5096cae25a63310c8ee7fb0feb1235e
GreenCurve <64778398+GreenCurve@users.noreply.github.com>
2021-04-11 01:31:29 +0300

    Update notsomain.py

--- notsomain.py
+++ notsomain.py
@@ -11,9 +11,22 @@ if option == 0:
     ligand = input('Ввод лиганда для докинга: ')
     water = input('Ввод колеблющейся воды: ')
     settings = input("Введите настройки: ")
-    dockingRepeats = int(input('Кол во докингов: '))
+    dockingRepeats = input('Кол во докингов: ')
+    if protein == '':
+        protein = '1dwb.pdb'
+    if ligand == '':
+        ligand = 'ligandSelf.mol'
+    if settings == '':
+        settings = 'settings.txt'
+    if dockingRepeats == '':
+        dockingRepeats = int(2)
+    dockingRepeats = int(dockingRepeats)
     Operation.Docking(protein, ligand, water, settings, dockingRepeats)
 elif option == 1:
     protein = input('Ввод белка для докинга: ')
     settings = input("Введите настройки: ")
+    if protein == '':
+        protein = '1dwb.pdb'
+    if settings == '':
+        settings = 'settings.txt'
     Operation.Extracting(protein, settings)
